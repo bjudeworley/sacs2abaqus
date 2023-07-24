c5c629ff55c858374903ff4ad9b4fcbc12f7647d
ben.jude@worley.com <ben.jude@worley.com>
2023-07-25 09:40:48 +1000

    Factor out the abaqus file writing into functions

--- convert_SACSv1_noDialog.py
+++ convert_SACSv1_noDialog.py
@@ -219,6 +219,234 @@ def parse_sacs_file(input_file: str, secondary_input: str = None):
     return stru
 
 
+
+def merge_small_members(stru: SacsStructure) -> None:
+    strip_count = 0
+    for m in stru.members.keys():
+        if (
+            GetDistance(
+                stru.joints[stru.members[m].jointA], stru.joints[stru.members[m].jointB]
+            )
+            < length_tol
+        ):
+            strip_count += 1
+            # Merge the end joints of this member to retain continuity in the model
+            # and record the merge in the nmap list
+            stru.nmap[stru.joints[stru.members[m].jointB].ABQID - 1].append(
+                "MERGED WITH {}".format(stru.joints[stru.members[m].jointA].ABQID)
+            )
+            stru.joints[stru.members[m].jointB].ABQID = stru.joints[
+                stru.members[m].jointA
+            ].ABQID
+    print("\n {} members were removed.".format(strip_count))
+
+
+
+def write_nodes(stru: SacsStructure, out):
+    out.write("*Node, nset=N-AllNodes\n")
+    for j in sorted(stru.joints.keys()):
+        out.write(
+            "{}, {}, {}, {}\n".format(
+                stru.joints[j].ABQID,
+                stru.joints[j].x,
+                stru.joints[j].y,
+                stru.joints[j].z,
+            )
+        )
+
+
+def write_elements(stru: SacsStructure, out):
+    elnum = 1
+    for g in stru.grups:
+        out.write(
+            "*Element, type={}, elset=MG-{}\n".format(beam_type, g.replace(".", "-"))
+        )
+        for m in stru.members:
+            if stru.members[m].group == g:
+                stru.members[m].ABQID = elnum
+                out.write(
+                    "{}, {}, {}\n".format(
+                        stru.members[m].ABQID,
+                        stru.joints[stru.members[m].jointA].ABQID,
+                        stru.joints[stru.members[m].jointB].ABQID,
+                    )
+                )
+                elnum += 1
+    for pg in stru.pgrups:
+        for p in stru.plates:
+            if stru.plates[p].group == pg:
+                stru.plates[p].ABQID = elnum
+                if stru.plates[p].jointD == "":
+                    # 3-element shell
+                    out.write(
+                        "*Element, type=S3, elset=PG-{}\n".format(pg.replace(".", "-"))
+                    )
+                    out.write(
+                        "{}, {}, {}, {}\n".format(
+                            stru.plates[p].ABQID,
+                            stru.joints[stru.plates[p].jointA].ABQID,
+                            stru.joints[stru.plates[p].jointB].ABQID,
+                            stru.joints[stru.plates[p].jointC].ABQID,
+                        )
+                    )
+                else:
+                    # 4-element shell
+                    out.write(
+                        "*Element, type=S4R, elset=PG-{}\n".format(pg.replace(".", "-"))
+                    )
+                    # Sometimes the order of joints in SACS results in a self-intersecting
+                    # element in Abaqus. Try to fix this assuming all plates are roughly
+                    # rectangular and checking that the next node defined is never the furthest away
+                    j1 = stru.joints[stru.plates[p].jointA]
+                    j2 = stru.joints[stru.plates[p].jointB]
+                    j3 = stru.joints[stru.plates[p].jointC]
+                    j4 = stru.joints[stru.plates[p].jointD]
+                    j1, j2, j3, j4 = OrderJoints([j1, j2, j3, j4])
+                    out.write(
+                        "{}, {}, {}, {}, {}\n".format(
+                            elnum, j1.ABQID, j2.ABQID, j3.ABQID, j4.ABQID
+                        )
+                    )
+                elnum += 1
+
+
+def write_sets(stru: SacsStructure, out):
+    out.write("****MEMBER ELEMENT SETS****\n")
+    for m in stru.members:
+        out.write(
+            "*Elset, elset=M-{}\n{}\n".format(
+                stru.members[m].ID.replace(".", "-"), stru.members[m].ABQID
+            )
+        )
+    out.write("****PLATE ELEMENT SETS****\n")
+    for p in stru.plates:
+        out.write(
+            "*Elset, elset=P-{}\n{}\n".format(
+                stru.plates[p].ID.replace(".", "-"), stru.plates[p].ABQID
+            )
+        )
+
+
+def write_beam_sections(stru: SacsStructure, out):
+    out.write("**\n** BEAM SECTIONS **\n")
+    # Have to assign sections to individual members, not groups, as we can not
+    # guarentee that all members in a group are vertical or non-vertical, so
+    # can't give a blanket orientation assignment
+    for m in stru.members:
+        g = stru.members[m].group
+        assigned = False
+        if g in stru.grups:
+            if stru.grups[g].section:
+                # This group uses a section definition: get values from that
+                try:
+                    s = stru.sects[stru.grups[g].section]
+                except KeyError:
+                    # This occurs if a SECT is specified in the GRUP, but there
+                    # is no corresponding SECT definition in the SACS file, so
+                    # use the section properties from the GRUP line instead.
+                    s = ""
+                except Exception as e:
+                    # Unexpected error, log
+                    logging.error(
+                        "ERROR: {} when trying to use section of GROUP: {}".format(e, g)
+                    )
+                if s:
+                    if s.sect in ["CON"]:
+                        logging.warning(
+                            "Members in group {} are assigned CON section; skipping\n".format(
+                                g
+                            )
+                        )
+                    elif s.sect in ["PIPE", "I", "TEE", "L", "CHL", "ARBITRARY", "BOX"]:
+                        out.write(s.abaqus_section_defn(m))
+                        assigned = True
+                    else:
+                        logging.warning(
+                            "Unknown section assignment in group {}, skipping\n.".format(
+                                g
+                            )
+                        )
+            else:
+                # This group defines its owns sects without a discrete SECT line
+                if stru.grups[g].OD != 0.0:
+                    # Pipe
+                    out.write(
+                        "*Beam Section, elset=M-{}, section={}, material=Mtl-Beam\n".format(
+                            m.replace(".", "-"), "PIPE"
+                        )
+                    )
+                    # Abaqus requires input of outside radii, whereas SACS is in OD
+                    out.write(
+                        "{}, {}\n".format(stru.grups[g].OD / 2, stru.grups[g].thickness)
+                    )  # outside radius, wall thickness
+                    assigned = True
+                else:
+                    logging.warning(
+                        "Unable to determine section properties for group {}, skipping.".format(
+                            g
+                        )
+                    )
+        else:
+            # Member group not found, report as error
+            stru.missing_sect_members.append(m)
+            logging.warning(
+                "No group definition found for member {}, no section can be assigned!".format(
+                    m.replace(".", "-")
+                )
+            )
+        if assigned:
+            start = Vector3(
+                stru.joints[stru.members[m].jointA].x,
+                stru.joints[stru.members[m].jointA].y,
+                stru.joints[stru.members[m].jointA].z,
+            )
+            end = Vector3(
+                stru.joints[stru.members[m].jointB].x,
+                stru.joints[stru.members[m].jointB].y,
+                stru.joints[stru.members[m].jointB].z,
+            )
+            beam_csys = BeamCSys.from_sacs_points(start, end).rotated_about_x(-90)
+            if stru.members[m].chordAngle:
+                beam_csys = beam_csys.rotated_about_x(stru.members[m].chordAngle)
+            local_z = beam_csys.z
+            out.write("{}, {}, {}\n".format(local_z.x, local_z.y, local_z.z))
+        else:
+            try:
+                stru.missing_sect_members.append(m)
+                logging.warning(
+                    "Missing section assignment for member {} (Group {}, Section ID {})\n".format(
+                        m.replace(".", "-"),
+                        stru.members[m].group,
+                        stru.grups[g].section,
+                    )
+                )
+            except Exception as e:
+                logging.error("** Unhandled error for group {}: {}\n".format(g, e))
+    out.write("*Material, name=Mtl-Beam\n*Density\n7850.,\n*Elastic\n2e+11, 0.3\n")
+
+    if stru.missing_sect_members:
+        out.write("*Elset, elset=ErrMissingSections\n")
+        for m in stru.missing_sect_members:
+            out.write("M-{}\n".format(m))
+        print(
+            "\n**NOTE: {} members have sections not defined in the SACS or Library files. These are added to sets ErrMissingSections\n".format(
+                len(stru.missing_sect_members)
+            )
+        )
+
+
+def write_plate_sections(stru: SacsStructure, out):
+    out.write("**\n** PLATE SECTIONS **\n")
+    for pg in stru.pgrups:
+        out.write(
+            "*Shell General Section, elset=PG-{}, material={}\n".format(
+                stru.pgrups[pg].ID.replace(".", "-"), "Mtl-Plate"
+            )
+        )
+        out.write("{}\n".format(stru.pgrups[pg].thickness))
+    out.write("*Material, name=Mtl-Plate\n*Density\n7850.,\n*Elastic\n2e+11, 0.3\n")
+
+
 stru = parse_sacs_file(file_list[0], file_list[1] if len(file_list) == 2 else None)
 print(
     "\nReading complete:\n\t"
@@ -229,225 +457,20 @@ print(
 )
 
 print("\nRemoving elements with length smaller than specified tolerance")
-strip_count = 0
-for m in stru.members.keys():
-    if (
-        GetDistance(
-            stru.joints[stru.members[m].jointA], stru.joints[stru.members[m].jointB]
-        )
-        < length_tol
-    ):
-        strip_count += 1
-        # Merge the end joints of this member to retain continuity in the model
-        # and record the merge in the nmap list
-        stru.nmap[stru.joints[stru.members[m].jointB].ABQID - 1].append(
-            "MERGED WITH {}".format(stru.joints[stru.members[m].jointA].ABQID)
-        )
-        stru.joints[stru.members[m].jointB].ABQID = stru.joints[
-            stru.members[m].jointA
-        ].ABQID
-print("\n {} members were removed.".format(strip_count))
+merge_small_members(stru)
 
 print("\nGenerating orphan mesh:")
-print("\tWriting nodes to input file...")
-try:
-    out = open(inp_file, "w")
-except:
-    print(
-        "Error trying to open "
-        + inp_file
-        + ", check that you have permission to do so. Exiting"
-    )
-    exit(2)
-
-out.write("*Node, nset=N-AllNodes\n")
-for j in sorted(stru.joints.keys()):
-    out.write(
-        "{}, {}, {}, {}\n".format(
-            stru.joints[j].ABQID, stru.joints[j].x, stru.joints[j].y, stru.joints[j].z
-        )
-    )
-
-print("\tWriting elements to input file...")
-elnum = 1
-for g in stru.grups:
-    out.write("*Element, type={}, elset=MG-{}\n".format(beam_type, g.replace(".", "-")))
-    for m in stru.members:
-        if stru.members[m].group == g:
-            stru.members[m].ABQID = elnum
-            out.write(
-                "{}, {}, {}\n".format(
-                    stru.members[m].ABQID,
-                    stru.joints[stru.members[m].jointA].ABQID,
-                    stru.joints[stru.members[m].jointB].ABQID,
-                )
-            )
-            elnum += 1
-for pg in stru.pgrups:
-    for p in stru.plates:
-        if stru.plates[p].group == pg:
-            stru.plates[p].ABQID = elnum
-            if stru.plates[p].jointD == "":
-                # 3-element shell
-                out.write(
-                    "*Element, type=S3, elset=PG-{}\n".format(pg.replace(".", "-"))
-                )
-                out.write(
-                    "{}, {}, {}, {}\n".format(
-                        stru.plates[p].ABQID,
-                        stru.joints[stru.plates[p].jointA].ABQID,
-                        stru.joints[stru.plates[p].jointB].ABQID,
-                        stru.joints[stru.plates[p].jointC].ABQID,
-                    )
-                )
-            else:
-                # 4-element shell
-                out.write(
-                    "*Element, type=S4R, elset=PG-{}\n".format(pg.replace(".", "-"))
-                )
-                # Sometimes the order of joints in SACS results in a self-intersecting
-                # element in Abaqus. Try to fix this assuming all plates are roughly
-                # rectangular and checking that the next node defined is never the furthest away
-                j1 = stru.joints[stru.plates[p].jointA]
-                j2 = stru.joints[stru.plates[p].jointB]
-                j3 = stru.joints[stru.plates[p].jointC]
-                j4 = stru.joints[stru.plates[p].jointD]
-                j1, j2, j3, j4 = OrderJoints([j1, j2, j3, j4])
-                out.write(
-                    "{}, {}, {}, {}, {}\n".format(
-                        elnum, j1.ABQID, j2.ABQID, j3.ABQID, j4.ABQID
-                    )
-                )
-            elnum += 1
-print("\t Generating Element Sets...")
-out.write("****MEMBER ELEMENT SETS****\n")
-for m in stru.members:
-    out.write(
-        "*Elset, elset=M-{}\n{}\n".format(
-            stru.members[m].ID.replace(".", "-"), stru.members[m].ABQID
-        )
-    )
-out.write("****PLATE ELEMENT SETS****\n")
-for p in stru.plates:
-    out.write(
-        "*Elset, elset=P-{}\n{}\n".format(
-            stru.plates[p].ID.replace(".", "-"), stru.plates[p].ABQID
-        )
-    )
-
-print("\tWriting beam section assignments to input file...")
-out.write("**\n** BEAM SECTIONS **\n")
-# Have to assign sections to individual members, not groups, as we can not
-# guarentee that all members in a group are vertical or non-vertical, so
-# can't give a blanket orientation assignment
-for m in stru.members:
-    g = stru.members[m].group
-    assigned = False
-    if g in stru.grups:
-        if stru.grups[g].section:
-            # This group uses a section definition: get values from that
-            try:
-                s = stru.sects[stru.grups[g].section]
-            except KeyError:
-                # This occurs if a SECT is specified in the GRUP, but there
-                # is no corresponding SECT definition in the SACS file, so
-                # use the section properties from the GRUP line instead.
-                s = ""
-            except Exception as e:
-                # Unexpected error, log
-                logging.error(
-                    "ERROR: {} when trying to use section of GROUP: {}".format(e, g)
-                )
-            if s:
-                if s.sect in ["CON"]:
-                    logging.warning(
-                        "Members in group {} are assigned CON section; skipping\n".format(
-                            g
-                        )
-                    )
-                elif s.sect in ["PIPE", "I", "TEE", "L", "CHL", "ARBITRARY", "BOX"]:
-                    out.write(s.abaqus_section_defn(m))
-                    assigned = True
-                else:
-                    logging.warning(
-                        "Unknown section assignment in group {}, skipping\n.".format(g)
-                    )
-        else:
-            # This group defines its owns sects without a discrete SECT line
-            if stru.grups[g].OD != 0.0:
-                # Pipe
-                out.write(
-                    "*Beam Section, elset=M-{}, section={}, material=Mtl-Beam\n".format(
-                        m.replace(".", "-"), "PIPE"
-                    )
-                )
-                # Abaqus requires input of outside radii, whereas SACS is in OD
-                out.write(
-                    "{}, {}\n".format(stru.grups[g].OD / 2, stru.grups[g].thickness)
-                )  # outside radius, wall thickness
-                assigned = True
-            else:
-                logging.warning(
-                    "Unable to determine section properties for group {}, skipping.".format(
-                        g
-                    )
-                )
-    else:
-        # Member group not found, report as error
-        stru.missing_sect_members.append(m)
-        logging.warning(
-            "No group definition found for member {}, no section can be assigned!".format(
-                m.replace(".", "-")
-            )
-        )
-    if assigned:
-        start = Vector3(
-            stru.joints[stru.members[m].jointA].x,
-            stru.joints[stru.members[m].jointA].y,
-            stru.joints[stru.members[m].jointA].z,
-        )
-        end = Vector3(
-            stru.joints[stru.members[m].jointB].x,
-            stru.joints[stru.members[m].jointB].y,
-            stru.joints[stru.members[m].jointB].z,
-        )
-        beam_csys = BeamCSys.from_sacs_points(start, end).rotated_about_x(-90)
-        if stru.members[m].chordAngle:
-            beam_csys = beam_csys.rotated_about_x(stru.members[m].chordAngle)
-        local_z = beam_csys.z
-        out.write("{}, {}, {}\n".format(local_z.x, local_z.y, local_z.z))
-    else:
-        try:
-            stru.missing_sect_members.append(m)
-            logging.warning(
-                "Missing section assignment for member {} (Group {}, Section ID {})\n".format(
-                    m.replace(".", "-"), stru.members[m].group, stru.grups[g].section
-                )
-            )
-        except Exception as e:
-            logging.error("** Unhandled error for group {}: {}\n".format(g, e))
-out.write("*Material, name=Mtl-Beam\n*Density\n7850.,\n*Elastic\n2e+11, 0.3\n")
-
-if stru.missing_sect_members:
-    out.write("*Elset, elset=ErrMissingSections\n")
-    for m in stru.missing_sect_members:
-        out.write("M-{}\n".format(m))
-    print(
-        "\n**NOTE: {} members have sections not defined in the SACS or Library files. These are added to sets ErrMissingSections\n".format(
-            len(stru.missing_sect_members)
-        )
-    )
-
-print("\tWriting plate section assignments to input file...")
-out.write("**\n** PLATE SECTIONS **\n")
-for pg in stru.pgrups:
-    out.write(
-        "*Shell General Section, elset=PG-{}, material={}\n".format(
-            stru.pgrups[pg].ID.replace(".", "-"), "Mtl-Plate"
-        )
-    )
-    out.write("{}\n".format(stru.pgrups[pg].thickness))
-out.write("*Material, name=Mtl-Plate\n*Density\n7850.,\n*Elastic\n2e+11, 0.3\n")
+with open(inp_file, "w") as out:
+    print("\tWriting nodes to input file...")
+    write_nodes(stru, out)
+    print("\tWriting elements to input file...")
+    write_elements(stru, out)
+    print("\t Generating Element Sets...")
+    write_sets(stru, out)
+    print("\tWriting beam section assignments to input file...")
+    write_beam_sections(stru, out)
+    print("\tWriting plate section assignments to input file...")
+    write_plate_sections(stru, out)
 
 print("\nWriting node number map to " + inp_file + "_nmap.txt...")
 try:
